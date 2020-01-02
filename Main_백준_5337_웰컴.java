a3c91a6cf4c05d038439926e702cf28d5000a685
gksmfwm00 <gksmfwm00@naver.com>
2020-01-03 02:14:30 +0900

    Create 5337.java

---
+++ Main_백준_5337_웰컴.java
@@ -0,0 +1,10 @@
+
+public class Main_백준_5337_웰컴 {
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(".  .   .\n");
+		sb.append("|  | _ | _. _ ._ _  _\n");
+		sb.append("|/\\|(/.|(_.(_)[ | )(/.\n");
+		System.out.print(sb.toString());
+	}
+}
