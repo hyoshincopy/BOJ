336f2c7aef807f59d3b25cb8eb43729f49cd2437
gksmfwm00 <gksmfwm00@naver.com>
2020-01-04 01:15:35 +0900

    Create 10171.java

---
+++ Main_백준_10171_고양이.java
@@ -0,0 +1,9 @@
+public class Main_백준_10171_고양이 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.print(" \\(__)|");
+		
+	}
+}
