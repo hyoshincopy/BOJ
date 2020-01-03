bedac4212d86704e3cea193c8b324301b951336a
gksmfwm00 <gksmfwm00@naver.com>
2020-01-03 14:42:30 +0900

    Create 11726.java

---
+++ Main_백준_11726_2xn타일링.java
@@ -0,0 +1,26 @@
+package DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_백준_11726_2xn타일링 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		int n = Integer.parseInt(st.nextToken());
+		
+		// d[i] = 2 x i 타일을 채우는 방법의 수
+		int[] d = new int[n+1];
+		d[0] = 1;
+		d[1] = 1;
+		
+		for(int i=2;i<n+1;i++){
+			d[i] = d[i-1] + d[i-2];
+			d[i] %= 10007;
+		}
+		
+		System.out.print(d[n]);
+	}
+}
