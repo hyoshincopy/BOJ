3ca9f2054f3055e065dff185aa15cc8fa86d7316
gksmfwm00 <gksmfwm00@naver.com>
2020-01-06 17:33:58 +0900

    Create 15552.java

---
+++ Main_백준_15552_빠른AB.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main_백준_15552_빠른AB {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int T = Integer.parseInt(st.nextToken());
+		while(T-- > 0){
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			int sum = A + B;
+			bw.write(sum + "\n");
+		}
+
+		bw.flush();
+	}
+}
