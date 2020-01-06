3cb0be94030cf73fb7f91dab2fd5207c25bd9f98
gksmfwm00 <gksmfwm00@naver.com>
2020-01-06 17:44:08 +0900

    Create 11021.java

---
+++ Main_백준_11021_AB_7.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_백준_11021_AB_7 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		int T = Integer.parseInt(st.nextToken());
+		for(int tc=1;tc<=T;tc++){
+			st = new StringTokenizer(br.readLine(), " ");
+
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			System.out.println("Case #" + tc + ": " + (A+B));
+		}
+	}
+}
