bedf1cb49367e5a5368d9e5794ab8dd2adde55e7
gksmfwm00 <gksmfwm00@naver.com>
2020-01-06 17:38:45 +0900

    Create 2742.java

---
+++ Main_백준_2742_기찍N.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main_백준_2742_기찍N {
+	public static void main(String[] args) {	
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		for(int i=N;1<=i;i--)
+			System.out.println(i);
+	}
+}
